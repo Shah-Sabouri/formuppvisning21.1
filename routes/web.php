9212431a318c45ba67188627768734952680b4fe
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-01-21 09:39:09 +0100

    postcontroller view made, etc

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('add-blog-post-form', [PostController::class, 'index']);
+Route::post('store-form', [PostController::class, 'store']);
